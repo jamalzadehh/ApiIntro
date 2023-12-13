be2738b0c2a9c3d256dfd113a3261b1acda9ed80
Your Name <you@example.com>
2023-12-14 01:20:14 +0400

    File uploaded

---
+++ ApiIntro/Controllers/CategoriesController.cs
@@ -0,0 +1,61 @@
+﻿using ApiIntro.DAL;
+using ApiIntro.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiIntro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(int page,int take)
+        {
+            List<Category> categories = await _context.Categories.Skip((page-1)*take).Take(take).ToListAsync();
+            return Ok(categories);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Category category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null) return NotFound();
+            return Ok(category);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created, category);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, string name)
+        {
+            if (id <= null) return BadRequest();
+            Category existed = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (existed == null) return NotFound();
+            existed.Name = name;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Category existed = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (existed == null) return NotFound();
+            _context.Categories.Remove(existed);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ ApiIntro/Controllers/TagsController.cs
@@ -0,0 +1,66 @@
+﻿using ApiIntro.DAL;
+using ApiIntro.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiIntro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TagsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(int page, int take)
+        {
+            List<Tag> tags = await _context.Tags.Skip((page - 1) * take).Take(take).ToListAsync();
+            return Ok(tags);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Tag tag = await _context.Tags.FirstOrDefaultAsync(x => x.Id == id);
+            if (tag == null) return NotFound();
+            return Ok(tag);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            await _context.Tags.AddAsync(tag);
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created, tag);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, string name)
+        {
+            if (id <= 0) return BadRequest();
+
+            Tag existed = await _context.Tags.FirstOrDefaultAsync(x => x.Id == id);
+            if (existed == null) return NotFound();
+            existed.Name = name;
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Tag existed = await _context.Tags.FirstOrDefaultAsync(x=>x.Id==id);
+            if (existed == null) return NotFound();
+            _context.Tags.Remove(existed);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+    }
+}
